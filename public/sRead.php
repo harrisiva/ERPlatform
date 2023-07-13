3b13837d331994bf431a22b30600af0101a9381d
Maham Farooq <mahamf100@gmail.com>
2023-07-12 20:57:58 -0400

    small styling edit

--- public/sRead.php
+++ public/sRead.php
@@ -4,11 +4,10 @@
     require "../packages/db.php";
     require "../templates/header.php";
 ?>
-
-
-
+<html>
+    <body>hi</body>
+</html>
 <?php
-
     function read ($conn){
         // prepared SQL statement
         $stmt =$conn->prepare("SELECT * FROM supplier");
@@ -17,21 +16,21 @@
         // creating tables with column names
         echo "<table>";
         echo "<tr>";
-        echo "<th style = 'padding-right:25px'>Supplier ID  </th>";
-        echo "<th style = 'padding-right:25px'>Supplier Name  </th>";
-        echo "<th style = 'padding-right:25px'>Address  </th>";
-        echo "<th style = 'padding-right:25px'>Phone  </th>";
-        echo "<th style = 'padding-right:25px'>Email  </th>";
+        echo "<th class = spacing>Supplier ID  </th>";
+        echo "<th class = spacing>Supplier Name  </th>";
+        echo "<th class = spacing>Address  </th>";
+        echo "<th class = spacing>Phone  </th>";
+        echo "<th class = spacing>Email  </th>";
         echo "</tr>";
 
         // fetches all sql entries in supplier table as rows
         while ($read = $stmt->fetch(PDO::FETCH_ASSOC)) {
             echo "<tr>";
-            echo "<td style = 'padding-right:25px'>" . $read['supplierID'] . "</td>";
-            echo "<td style = 'padding-right:25px'>" . $read['supplierName'] . "</td>";
-            echo "<td style = 'padding-right:25px'>" . $read['address'] . "</td>";
-            echo "<td style = 'padding-right:25px'>" . $read['phone'] . "</td>";
-            echo "<td style = 'padding-right:25px'>" . $read['email'] . "</td>";
+            echo "<td class = spacing>" . $read['supplierID'] . "</td>";
+            echo "<td class = spacing>" . $read['supplierName'] . "</td>";
+            echo "<td class = spacing>" . $read['address'] . "</td>";
+            echo "<td class = spacing>" . $read['phone'] . "</td>";
+            echo "<td class = spacing>" . $read['email'] . "</td>";
             echo "</tr>";
         }
         echo "</table>";
@@ -44,3 +43,8 @@
     require "../templates/footer.php";
 
 ?>
+<style>
+.spacing{
+    padding-right:25px;
+}
+</style>
