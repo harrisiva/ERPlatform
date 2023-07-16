b40633ce00d2d2ade83273c599a0f6d35ae61518
harrisiva <harrishiv6@gmail.com>
2023-07-16 04:20:06 -0400

    Refactored to create includes folder (and to create login.inc.php, logout.inc.php, and signup.inc.php - files that contain the php code that performs the required functions

--- packages/db.php
+++ packages/db.php
@@ -152,21 +152,3 @@
 
 
 ?>
-
-<!-- driver/test code -->
-<?php 
-
-// Load ENV variables and setup the required info for establishing a DB connection
-$host = 'mydb.cbbhaex7aera.us-east-2.rds.amazonaws.com';
-$name = 'CP476_Project';
-$username = 'admin';
-$password = 'cp476-%uni';
-
-$handler = new Products($host, $name, $username, $password); // Create Products Handler (inherited from the DB Handler/PDO Wrapper)
-
-$products_test_data = array(
-    array(0004, 'Product', 'Another Thing', 799.9, 50, 'B', 7890),
-);
-echo "DB.PHP Driver Code: <br>";
-
-?>
