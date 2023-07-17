39849c79e6af54e7e63b48096ff894ea26a6aebd
harrisiva <harrishiv6@gmail.com>
2023-07-17 10:19:35 -0400

    Fixed bug with order of require imports

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -2,8 +2,8 @@
 
 if (isset($_POST["submit"])) { // Run code only if it was called with POST method (and submit button)
     // Instantiate loginContr class (db User) 
+    require "../classes/signup.classes.php";
     require "../classes/signup-contr.classes.php";
-    //require "../classes/signup.classes.php";
     
     // Get data from POST request
     $uname = $_POST["uname"];
@@ -11,7 +11,7 @@ if (isset($_POST["submit"])) { // Run code only if it was called with POST metho
     $pwdRepeat = $_POST["pwdrepeat"]; 
 
     // Running error handlers and user sign up function (extends DB)
-    //$controller = new SignupContr($uname, $pwd, $pwdRepeat); 
+    $controller = new SignupContr($uname, $pwd, $pwdRepeat); 
     //$controller->signupUser();
 
     // Return back to front page 
