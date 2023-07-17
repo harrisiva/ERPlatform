bda1ce2f904128bae35d7c040a8dd78b36f0b27b
harrisiva <harrishiv6@gmail.com>
2023-07-17 11:17:49 -0400

    Setup shell login inc.php and classes.php files

---
+++ classes/login.classes.php
@@ -0,0 +1,16 @@
+<?php 
+require "../packages/db.php";
+
+class Login {
+
+    function getUser($username, $password) {
+        $handler = new Database();
+        $response = $handler->read (
+            query: "SELECT * FROM users WHERE username=?;", values: array($username)
+        );
+        if (count($response)==0) {header("location: ../index.php?error=userNotFound");exit();};
+        echo var_dump($response);
+        $checkPwd = password_verify($password, $response[1]);
+        return $response; 
+    }
+}
--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,12 +1,15 @@
 <?php 
 
 if (isset($_POST["submit"])) { // Run code only if it was called with POST method (and submit button)
-    $uid = $_POST["uid"];
+    require "../classes/login-contr.classes.php"; // Instantiate loginContr class (db User) 
+    
+    // Get data from POST request
+    $uname = $_POST["uname"];
     $pwd = $_POST["pwd"]; 
 
-    // Instantiate loginContr class (db User)
-
-    // Running error handlers and user login
+    // Running error handlers and user sign up function (extends DB)
+    $controller = new LoginContr($uname, $pwd); 
+    $controller->loginUser(); // Instantiate loginContr class (db User)
 
     // Return back to front page 
     header("location: ../index.php?error=none");
