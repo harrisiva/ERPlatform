ee6c103bc64de22bffac1ca6ef0a6b2dfae02944
harrisiva <harrishiv6@gmail.com>
2023-07-17 11:27:44 -0400

    Updated login password matching check

--- classes/login.classes.php
+++ classes/login.classes.php
@@ -4,14 +4,19 @@ require "../packages/db.php";
 class Login {
 
     function getUser($username, $password) {
+        
+        // Get entry matching username
         $handler = new Database();
         $response = $handler->read (
             query: "SELECT * FROM users WHERE username=?;", values: array($username)
         );
         if (count($response)==0) {header("location: ../index.php?error=userNotFound");exit();};
-        echo "Response from login getUser(): <br>";
-        echo var_dump($response);
-        //$checkPwd = password_verify($password, $response[1]);
+        
+        // Check if given password matches with password for given username in the DB entry
+        $checkPwd=false;
+        if ($password==$response["password"]) {$checkPwd=True;};
+
+        // Setup session and exit
         return $response; 
     }
 }
